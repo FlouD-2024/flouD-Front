0c7f1dc78b55e0a4287c8cbd66efab4a5adf3f72
minibxx <alsdbqls1004@gmail.com>
2024-02-28 17:35:19 +0900

    feat: 마이페이지 친구 목록 파트 구현중

--- src/components/mypage/Friends.tsx
+++ src/components/mypage/Friends.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import FriendList from './FriendList'
 import FriendsAdd from './FriendsAdd'
+import UnFriend from './UnFriend'
 
 type Props = {}
 
 export default function Friends({}: Props) {
   return (
-    <>
+    <div className='relative'>
         <FriendsAdd/>
         <FriendList/>
-    </>
+        <UnFriend/>
+    </div>
   )
 }
---
+++ src/components/mypage/UnFriend.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from "next/image";
+import UnFriendImg from '@/img/mypage/UnFriend.png'
+
+type Props = {
+  name: string
+}
+
+export default function UnFriend({name}: Props) {
+  return (
+    <>
+        <div className='bg-[#000000]/10 absolute w-[100%] h-[100%]'>
+            <div className='flex-col bg-[white] w-[450px] h-[370px] rounded-[50px] text-center pb-[25px] pt-[55px] m-auto'>
+                <Image src={UnFriendImg} className='ml-auto mr-auto mb-[10px]'/>
+                <div className='text-[20px] font-bold my-[5px]'>{name}님과 친구를 <br/>끊으시겠습니까? </div>
+                <div className='text-[#828282] text-[14px]'>더 이상 서로 회고를 열람할 수 없습니다.</div>
+                <button className='text-[20px] bg-[black] text-white w-[110px] h-[40px] rounded-[50px] mt-[30px] mr-[10px]'>네</button>
+                <button className='text-[20px] bg-[#4C6FFF] text-white w-[110px] h-[40px] rounded-[50px]'>아니오</button>
+            </div>
+        </div>
+    </>
+  )
+}
