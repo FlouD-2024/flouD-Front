141affea5f6cfd6130eb4fe8788afb625bb894be
Ariling <97501226+Ariling@users.noreply.github.com>
2024-03-01 16:40:54 +0900

    feat : sidebar 구현

--- src/components/util/Aside.tsx
+++ src/components/util/Aside.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import tw, { css, styled } from "twin.macro";
 import aside from "@/img/aside/목록 로고.png";
 import Image from "next/image";
@@ -11,9 +11,18 @@ import MypageSvg from "@/img/svg/aside/Mypage.svg";
 import AboutSvg from "@/img/svg/aside/About.svg";
 import MainUnClick from "@/img/svg/aside/MainUnclick.svg";
 import FriendUnclick from "@/img/svg/aside/FriendUnClick.svg";
+import KPTUnclick from "@/img/svg/aside/KPTUnClick.svg";
+import CommunityUnclick from "@/img/svg/aside/CommunityUnclick.svg";
+import AboutClick from "@/img/svg/aside/AboutClick.svg";
+import MypageUnClick from "@/img/svg/aside/MypageUnclick.svg";
+import MypageToggle from "@/img/svg/aside/MypageToggle.svg";
 
 const Aside = () => {
   const router = useRouter();
+  const [mypageInfo, setMypageInfo] = useState("info");
+  const onClick = (str: string) => {
+    setMypageInfo(str);
+  };
   return (
     <SideBarWrapper>
       <div className="mt-[42px] mb-[33.3px]">
@@ -43,7 +52,7 @@ const Aside = () => {
           router.push("/");
         }}
       >
-        <KPT />
+        {router.pathname === "/" ? <KPT /> : <KPTUnclick />}
         <div>회고</div>
       </SideBarDiv>
       <SideBarDiv
@@ -52,25 +61,62 @@ const Aside = () => {
           router.push("/community");
         }}
       >
-        <Community />
+        {router.pathname === "/community" ? (
+          <Community />
+        ) : (
+          <CommunityUnclick />
+        )}
         <div>커뮤니티</div>
       </SideBarDiv>
+      {/* 여기 이름도 바뀔예정 */}
       <SideBarDiv
-        routerInfo={router.pathname === "/mypage"}
+        routerInfo={router.pathname === "/"}
         onClick={() => {
           router.push("/mypage");
         }}
       >
-        <MypageSvg />
+        {/* 바뀌어야 되는데 안 바뀌어서 여기서 테스트 */}
+        {router.pathname === "/" ? <MypageSvg /> : <MypageUnClick />}
         <div>마이페이지</div>
+        <div className=" absolute left-[85%]">
+          {router.pathname === "/" ? <MypageToggle /> : null}
+        </div>
       </SideBarDiv>
+      {router.pathname === "/" ? (
+        <MypageDiv>
+          <MypageText
+            name={mypageInfo === "info"}
+            onClick={() => onClick("info")}
+          >
+            내 정보 수정
+          </MypageText>
+          <MypageText
+            name={mypageInfo === "confirm"}
+            onClick={() => onClick("confirm")}
+          >
+            작성글 확인
+          </MypageText>
+          <MypageText
+            name={mypageInfo === "friend"}
+            onClick={() => onClick("friend")}
+          >
+            친구 관리
+          </MypageText>
+          <MypageText
+            name={mypageInfo === "talk"}
+            onClick={() => onClick("talk")}
+          >
+            문의하기
+          </MypageText>
+        </MypageDiv>
+      ) : null}
       <SideBarDiv
         routerInfo={router.pathname === "/about"}
         onClick={() => {
           router.push("/");
         }}
       >
-        <AboutSvg />
+        {router.pathname === "/about" ? <AboutClick /> : <AboutSvg />}
         <div>About FlouD</div>
       </SideBarDiv>
     </SideBarWrapper>
@@ -98,7 +144,7 @@ export const SideBarWrapper = styled.div`
 export const SideBarDiv = styled.div(
   ({ routerInfo }: { routerInfo: boolean }) => [
     tw`
-        w-full h-10 flex items-center gap-[11px] py-3 px-9 text-[17px] tracking-[-6%] cursor-pointer
+        w-full h-10 relative flex items-center gap-[11px] py-3 px-9 text-[17px] tracking-[-6%] cursor-pointer
     `,
     routerInfo
       ? css`
@@ -111,3 +157,30 @@ export const SideBarDiv = styled.div(
         `,
   ]
 );
+
+const MypageDiv = styled.div`
+  height: 183px;
+  width: 100%;
+  text-align: right;
+  background-color: rgba(76, 111, 255, 0.09);
+  border-right: 3px solid #4c6fff;
+  margin-top: -10px;
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  padding: 10px 26px 10px 0px;
+`;
+
+const MypageText = styled.div(({ name }: { name: boolean }) => [
+  css`
+    color: #a2aaca;
+    font-size: 17px;
+    letter-spacing: -6%;
+    font-weight: 500;
+  `,
+  name
+    ? css`
+        color: black;
+      `
+    : css``,
+]);
