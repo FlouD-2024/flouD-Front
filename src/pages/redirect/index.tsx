b717bc1dfd9396641e6af63f6058e77f32467bf5
Ariling <97501226+Ariling@users.noreply.github.com>
2024-03-14 13:43:37 +0900

    fix : 로그인 연결에 따른 불필요한 페이지 제거 및 코드 수정

--- src/pages/redirect/index.tsx
+++ src/pages/redirect/index.tsx
@@ -5,8 +5,8 @@ import React, { useEffect } from "react";
 const AuthPage = () => {
   const router = useRouter();
   useEffect(() => {
-    const accessToken = router.query.accesstoken as string;
-    const refreshToken = router.query.refreshtoken as string;
+    const accessToken = router.query.access_token as string;
+    const refreshToken = router.query.refresh_token as string;
     setAccessAndRefresh(accessToken, refreshToken);
     router.replace("/main");
   });
